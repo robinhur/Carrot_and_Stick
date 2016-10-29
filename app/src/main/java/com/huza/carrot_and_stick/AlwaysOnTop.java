42b35cf6e70faf74c491b8493e9e4f9dd876222d
desktop <robinhur@naver.com>
2016-10-30 01:36:45 +0900

    chanegd AOT_screen visibility(hide navigation)

--- app/src/main/java/com/huza/carrot_and_stick/AlwaysOnTop.java
+++ app/src/main/java/com/huza/carrot_and_stick/AlwaysOnTop.java
@@ -8,7 +8,6 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.graphics.PixelFormat;
-import android.os.Build;
 import android.os.Handler;
 import android.os.IBinder;
 import android.support.v4.view.PagerAdapter;
@@ -95,7 +94,7 @@ public class AlwaysOnTop extends Service {
         aot_indicator = (DotIndicator) OnTop_view.findViewById(R.id.AOT_indicator);
         aot_indicator.setSelectedDotColor(Color.parseColor("#FFFFFF"));
         aot_indicator.setSelectedDotDiameterDp(10);
-        aot_indicator.setUnselectedDotColor(Color.parseColor("#595959"));
+        aot_indicator.setUnselectedDotColor(Color.parseColor("#888888"));
         aot_indicator.setNumberOfItems(aot_adapter.getCount());
         aot_indicator.setSelectedItem(1, false);
 
@@ -128,11 +127,11 @@ public class AlwaysOnTop extends Service {
                 WindowManager.LayoutParams.FLAG_WATCH_OUTSIDE_TOUCH,
                 PixelFormat.TRANSLUCENT);
 
-        final int ui_Options = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                | View.SYSTEM_UI_FLAG_FULLSCREEN
+        final int ui_Options = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                //| View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                //| View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                //| View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                //| View.SYSTEM_UI_FLAG_FULLSCREEN
                 | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
 
         OnTop_view.setSystemUiVisibility(ui_Options);
