6ca1440c6b9e02c40e1be210c544958e954c1c71
desktop <robinhur@naver.com>
2016-11-01 05:19:38 +0900

    typo ^^;

--- app/src/main/java/com/huza/carrot_and_stick/AlwaysOnTop.java
+++ app/src/main/java/com/huza/carrot_and_stick/AlwaysOnTop.java
@@ -441,10 +441,7 @@ public class AlwaysOnTop extends Service {
             switch (msg.what) {
                 case 1:
                     Log.d(PACKAGE_NAME, "AlwaysOnTop : IncomingHandler : 화면 설정!!!");
-                    //OnTop_view.setSystemUiVisibility(ui_Options);
-
-                    //w_manager = (WindowManager) getSystemService(WINDOW_SERVICE);
-                    //w_manager.updateViewLayout(OnTop_view, params);
+                    OnTop_view.setSystemUiVisibility(2);
 
                     Log.d(PACKAGE_NAME, "AlwaysOnTop : IncomingHandler : Main Card 선택!!!");
                     aot_indicator.setSelectedItem(1, true);
